7486df1061bf9823792add1e0571fca9815906da
rcski77 <rcski77@gmail.com>
2022-06-10 15:29:11 -0400

    removed extra fbhelper

--- components/fb-helper.js
+++
@@ -1,4 +0,0 @@
-import { getDatabase, onValue, push, ref, remove, set } from "firebase/database";
-
-import { firebaseConfig } from "./fb-credentials";
-import { initializeApp } from "firebase/app";
