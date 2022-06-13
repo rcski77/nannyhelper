c19fb48ad53737e2166d6888c11cb4f3ea3ce21f
Khiem H Nguyen <khnguyen88@gmail.com>
2022-06-13 00:01:41 -0400

    WIP: Successful Firebase Storage Integration

--- helpers/fb_storage_helper.js
+++ helpers/fb_storage_helper.js
@@ -1,6 +1,51 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./fb_creds";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export function initDB() {
   initializeApp(firebaseConfig);
+  console.log("SUCCESS");
+}
+
+export async function upload(fileURI, userID, setLoading, setImageURI) {
+  console.log("FLAG 0");
+  const storageFB = getStorage();
+
+  console.log("FLAG 1");
+  const uriName = "profiles/profile-pic-" + userID + ".jpg";
+  const fileRef = ref(storageFB, uriName);
+
+  console.log("FLAG 2");
+  // A) convert image to bytes. Potential Key to avoiding transferring image binary from one screen to another as commented in profile settings
+  // A1) access image from image uri
+  const image = await fetch(fileURI);
+
+  console.log("FLAG 3");
+  // A2) convert image into an array of bytes, or blob object
+  const bytes = await image.blob();
+
+  console.log("FLAG 4");
+  // Toggle on loading flag (use State) to enable Buttons
+  setLoading(true);
+
+  console.log("FLAG 5");
+  //Uploads image onto firebase
+  const snapshot = await uploadBytes(fileRef, bytes);
+
+  console.log("FLAG 6");
+  // Gets imageURL uploaded on firebase
+  const photoURL = await getDownloadURL(fileRef);
+
+  console.log("FLAG 7");
+  //Set new imageURI based on Firebase Location
+  setImageURI(photoURL);
+
+  console.log("FLAG 8");
+  // Toggle on loading flag (use State) to disable Buttons
+  setLoading(false);
+
+  console.log("FLAG 9");
+  //Notify upload is complete
+  alert("File Uploaded!");
+  console.log(photoURL);
 }
